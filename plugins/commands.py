61df843d42fad065da370096656dcaab046b745f
ZINAN <97530806+Zinan100@users.noreply.github.com>
2023-07-27 23:32:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,17 +1,26 @@
 from pyogram import Client as app, filters
 from pyrogram.types import InlineKeyboardMarkup as Markup, InlineKeyboardButton as Button
+from Script import *
+
 
 @app.on_message(filters.command(start))
-async def start(bot, msg):
-  if not await db.get_chat(message.chat.id):
-            total=await client.get_chat_members_count(message.chat.id)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
-            await db.add_chat(message.chat.id, message.chat.title)
+#kanged from eva maria
+if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        buttons = [[
+                    Button("Updates", url="t.me/zib_bots")
+        reply_markup = Markup(buttons)
+        await message.reply_text(
+            text=GSTART_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML,
+            disable_web_page_preview=True
+        )
+        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         return 
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
-  hu = await bot.get_me()
+    if len(message.command) != 2:  hu = await bot.get_me()
   btn = [[
     Button('Channel', url='zib_bots'),
     Button('Group', url='zib_discussion')
