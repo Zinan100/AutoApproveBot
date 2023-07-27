3061f9aea1771df001b3faa1d58276c35bcb24ca
ZINAN <97530806+Zinan100@users.noreply.github.com>
2023-07-27 21:42:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+
+Client = app(
+  "Auto Req Acceptor Bot",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=BOT_TOKEN,
+  plugins={"root": "plugins"}
+)
+
+
+
+
+app.run()
